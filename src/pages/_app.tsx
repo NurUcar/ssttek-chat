46e3ecace7d1456103365921d74166b0ed88a4ab
NurUcar <nur0ucar@gmail.com>
2023-04-29 14:27:42 +0300

    feat: project structure

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import "@/assets/styles/globals.css";
+import { wrapper } from "@/store/store";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />;
+}
+
+export default wrapper.withRedux(MyApp);
